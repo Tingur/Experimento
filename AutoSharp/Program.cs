3cd96af5b0a713b29a9866042784963c24bafbdb
Tingur <entretenimentotubergamer@gmail.com>
2016-03-05 22:54:35 -0300

    Update Program.cs

--- AutoSharp/Program.cs
+++ AutoSharp/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using AutoSharp.Auto;
-using AutoSharp.Utils;
 using EloBuddy;
 using EloBuddy.SDK;
 using EloBuddy.SDK.Menu;
