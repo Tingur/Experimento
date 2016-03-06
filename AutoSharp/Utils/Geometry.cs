a15f5e29c3d1367fbf08f36b74794d0f2b7e4327
Tingur <entretenimentotubergamer@gmail.com>
2016-03-05 23:26:43 -0300

    Update Geometry.cs

--- AutoSharp/Utils/Geometry.cs
+++ AutoSharp/Utils/Geometry.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using ClipperLib;
 using EloBuddy;
-using EloBuddy.SDK;
+//using EloBuddy.SDK;
 using SharpDX;
 using Color = System.Drawing.Color;
 using Path = System.Collections.Generic.List<ClipperLib.IntPoint>;
@@ -16,7 +16,7 @@ namespace AutoSharp.Utils
 
         public static void DrawLineInWorld(Vector3 start, Vector3 end, int width, Color color)
         {
-            //var from = Drawing.WorldToScreen(start);
+            var from = Drawing.WorldToScreen(start);
             var to = Drawing.WorldToScreen(end);
             Drawing.DrawLine(from[0], from[1], to[0], to[1], width, color);
             //Drawing.DrawLine(from.X, from.Y, to.X, to.Y, width, color);
